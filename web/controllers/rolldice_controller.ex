3513c3fd18844a63f9795f785267d96ffef37249
Thomas Quigley Malcolm <tqmalcolm@gmail.com>
2017-11-30 21:51:17 -0600

    Rolling dice with recursion

--- web/controllers/rolldice_controller.ex
+++ web/controllers/rolldice_controller.ex
@@ -4,17 +4,25 @@ defmodule Thegm.RollDiceController do
   alias Thegm.RollDice
 
   def index(conn, %{"dice" => dice}) do
-    result_dice = []
-    Enum.each dice, fn die ->
-      split = String.split(die, "d", parts: 2)
-      {number, _rem} = Integer.parse(Enum.at(split, 0))
-      {sides, _rem} = Integer.parse(Enum.at(split, 1))
-      results = for n <- 1..number, do: :rand.uniform(sides)
-      result_dice = result_dice ++ [%{:type => "d" <> Enum.at(split, 1), :count => number, :rolls => results}]
-    end
+    result_dice = eat_dice(dice)
 
     conn
     |> put_status(:ok)
-    #|> render("index.json", dice: result_dice)
+    render conn, "index.json", dice: result_dice
   end
+
+  def eat_dice([]) do
+    []
+  end
+
+  def eat_dice([head | tail]) do
+    split = String.split(head, "d", parts: 2)
+    {number, _rem} = Integer.parse(Enum.at(split, 0))
+    {sides, _rem} = Integer.parse(Enum.at(split, 1))
+    results = for n <- 1..number, do: :rand.uniform(sides)
+    short = %{:type => "d" <> Enum.at(split, 1), :count => number, :rolls => results}
+    result_dice = [short | eat_dice(tail)]
+    result_dice
+  end
+
 end
