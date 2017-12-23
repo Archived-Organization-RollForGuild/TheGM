9a6ac45e16d9a5015212176da88485c106109f8c
Thomas Quigley Malcolm <tqmalcolm@gmail.com>
2017-12-23 16:44:10 -0600

    Removing unneeded aliases

--- web/controllers/betasub_controller.ex
+++ web/controllers/betasub_controller.ex
@@ -1,15 +1,13 @@
 defmodule Thegm.BetasubController do
   use Thegm.Web, :controller
 
-  alias Thegm.Betasub
-
   def create(conn, %{"email" => email}) do
     # email
     {:ok, final} = Enum.fetch(Enum.filter(Mailchimp.Account.get! |> Mailchimp.Account.lists!, fn(x) -> Map.get(x, :name) == "Roll For Guild" end), 0)
     case Mailchimp.List.create_member(final, email, :subscribed, %{}, %{}) do
       {:ok, _resp} ->
         send_resp(conn, :created, "")
-      {:error, resp} ->
+      {:error, _resp} ->
         conn
         |> put_status(:bad_request)
         |> render("") #TODO
--- web/controllers/rolldice_controller.ex
+++ web/controllers/rolldice_controller.ex
@@ -1,8 +1,6 @@
 defmodule Thegm.RollDiceController do
   use Thegm.Web, :controller
 
-  alias Thegm.RollDice
-
   def index(conn, %{"dice" => dice}) do
     result_dice = eat_dice(dice)
 
@@ -19,7 +17,7 @@ defmodule Thegm.RollDiceController do
     split = String.split(head, "d", parts: 2)
     {number, _rem} = Integer.parse(Enum.at(split, 0))
     {sides, _rem} = Integer.parse(Enum.at(split, 1))
-    results = for n <- 1..number, do: :rand.uniform(sides)
+    results = for _n <- 1..number, do: :rand.uniform(sides)
     short = %{:type => "d" <> Enum.at(split, 1), :count => number, :rolls => results}
     result_dice = [short | eat_dice(tail)]
     result_dice
