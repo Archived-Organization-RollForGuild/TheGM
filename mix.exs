6bbf1b7beba610da5565241725b6660290cd6c78
Thomas Quigley Malcolm <tqmalcolm@gmail.com>
2017-12-28 17:07:07 -0600

    updating the mix.exs version definition to match the soon to be tag version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Thegm.Mixfile do
   def project do
     [
       app: :thegm,
-      version: "0.0.1",
+      version: "0.0.3",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
