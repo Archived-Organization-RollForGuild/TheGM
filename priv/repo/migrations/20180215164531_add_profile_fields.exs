9185daad954539b9648b545e3af389ac07976775
Thomas Quigley Malcolm <tqmalcolm@gmail.com>
2018-02-19 12:20:04 -0600

    patch to remove duplicate code

--- priv/repo/migrations/20180215164531_add_profile_fields.exs
+++ priv/repo/migrations/20180215164531_add_profile_fields.exs
@@ -6,8 +6,5 @@ defmodule Thegm.Repo.Migrations.AddProfileFields do
       add :avatar, :boolean, null: false, default: false
       add :bio, :string, size: 511
     end
-
-    drop unique_index(:groups, [:name])
-    create unique_index(:groups, [:slug])
   end
 end
