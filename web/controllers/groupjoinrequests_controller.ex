23a1941de8768f4dfc477243d1ee84a0410e1f6c
Alex S. Glomsaas <alex@sorlie.co.uk>
2018-02-21 19:21:44 +0100

    Implement ability for a user to leave a group or for group admin to remove a user resolves #29

--- web/controllers/groupjoinrequests_controller.ex
+++ web/controllers/groupjoinrequests_controller.ex
@@ -4,7 +4,7 @@ defmodule Thegm.GroupJoinRequestsController do
   alias Thegm.GroupJoinRequests
 
   # NOTE: This can probably be made prettier somehow, it isn't very readable currently - Quigley
-  def create(conn, %{"group_id" => group_id}) do
+  def create(conn, %{"groups_id" => group_id}) do
     user_id = conn.assigns[:current_user].id
     case Repo.one(from b in Thegm.GroupBlockedUsers, where: b.group_id == ^group_id and b.user_id == ^user_id and b.rescinded == false) do
       nil ->
@@ -86,7 +86,7 @@ defmodule Thegm.GroupJoinRequestsController do
     end
   end
 
-  def update(conn, %{"group_id" => group_id, "status" => status}) do
+  def update(conn, %{"groups_id" => group_id, "status" => status}) do
 
   end
 
--- web/controllers/groupmembers_controller.ex
+++ web/controllers/groupmembers_controller.ex
@@ -2,8 +2,9 @@ defmodule Thegm.GroupMembersController do
   use Thegm.Web, :controller
 
   alias Thegm.GroupMembers
+  alias Thegm.Groups
 
-  def index(conn, %{"group_id" => group_id}) do
+  def index(conn, %{"groups_id" => group_id}) do
     user_id = conn.assigns[:current_user].id
     case Repo.all(from m in GroupMembers, where: m.groups_id == ^group_id) |> Repo.preload(:users) do
       nil ->
@@ -20,6 +21,50 @@ defmodule Thegm.GroupMembersController do
             conn
             |> put_status(:forbidden)
             |> render(Thegm.ErrorView, "error.json", errors: ["You must be a member of the group to view the members"])
+        end
+    end
+  end
+
+  def delete(conn, %{"groups_id" => group_id, "id" => user_id}) do
+    current_user_id = conn.assigns[:current_user].id
+
+    case Repo.get(Groups, group_id) |> Repo.preload(:group_members) do
+      nil ->
+        conn
+        |> put_status(:not_found)
+        |> render(Thegm.ErrorView, "error.json", errors: ["Group not found, maybe you already deleted it?"])
+      group ->
+        case Enum.find(group.group_members, fn x -> x.users_id == current_user_id end) do
+          member ->
+            if member.role != "admin" && current_user_id != user_id do
+              conn
+              |> put_status(:forbidden)
+              |> render(Thegm.ErrorView, "error.json", errors: ["You do not have permission to remove this user"])
+            end
+        end
+
+        case Enum.find(group.group_members, fn x -> x.users_id == user_id end) do
+          nil ->
+            conn
+            |> put_status(:not_found)
+            |> render(Thegm.ErrorView, "error.json", errors: ["members: Is not a member of the group"])
+          member ->
+            cond do
+              member.role != "admin" ->
+                case Repo.delete(member) do
+                  {:ok, _} ->
+                    send_resp(conn, :no_content, "")
+                  {:error, changeset} ->
+                    conn
+                    |> put_status(:internal_server_error)
+                    |> render(Thegm.ErrorView, "error.json", errors: Enum.map(changeset.errors, fn {k, v} -> Atom.to_string(k) <> ": " <> elem(v, 0) end))
+                end
+
+              member.role == "admin" ->
+                conn
+                |> put_status(:forbidden)
+                |> render(Thegm.ErrorView, "error.json", errors: ["Admins cannot be removed from the group"])
+            end
         end
     end
   end
