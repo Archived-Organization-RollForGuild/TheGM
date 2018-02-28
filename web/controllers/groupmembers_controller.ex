28793d365c12f64624276618e4418ceccc4d2125
Alex S. Glomsaas <alex@sorlie.co.uk>
2018-02-28 17:37:43 +0100

    Refactor leave group to avoid generating multiple responses in the same request

--- web/controllers/groupmembers_controller.ex
+++ web/controllers/groupmembers_controller.ex
@@ -34,36 +34,40 @@ defmodule Thegm.GroupMembersController do
         |> put_status(:not_found)
         |> render(Thegm.ErrorView, "error.json", errors: ["Group not found, maybe you already deleted it?"])
       group ->
-        case Enum.find(group.group_members, fn x -> x.users_id == current_user_id end) do
-          member ->
-            if member.role != "admin" && current_user_id != user_id do
-              conn
-              |> put_status(:forbidden)
-              |> render(Thegm.ErrorView, "error.json", errors: ["You do not have permission to remove this user"])
-            end
-        end
 
-        case Enum.find(group.group_members, fn x -> x.users_id == user_id end) do
-          nil ->
+        current_user_member = Enum.find(group.group_members, fn x -> x.users_id == current_user_id end)
+        target_member = Enum.find(group.group_members, fn x -> x.users_id == user_id end)
+
+        cond do
+          target_member == nil ->
             conn
             |> put_status(:not_found)
             |> render(Thegm.ErrorView, "error.json", errors: ["members: Is not a member of the group"])
-          member ->
-            cond do
-              member.role != "admin" ->
-                case Repo.delete(member) do
-                  {:ok, _} ->
-                    send_resp(conn, :no_content, "")
-                  {:error, changeset} ->
-                    conn
-                    |> put_status(:internal_server_error)
-                    |> render(Thegm.ErrorView, "error.json", errors: Enum.map(changeset.errors, fn {k, v} -> Atom.to_string(k) <> ": " <> elem(v, 0) end))
-                end
 
-              member.role == "admin" ->
+          current_user_member == nil ->
+            conn
+            |> put_status(:not_found)
+            |> render(Thegm.ErrorView, "error.json", errors: ["members: You are not a member of the group"])
+
+          current_user_member.role != "admin" && current_user_id != user_id ->
+            conn
+            |> put_status(:forbidden)
+            |> render(Thegm.ErrorView, "error.json", errors: ["You do not have permission to remove this user"])
+
+
+          target_member.role == "admin" ->
+            conn
+            |> put_status(:forbidden)
+            |> render(Thegm.ErrorView, "error.json", errors: ["Admins cannot be removed from the group"])
+
+          true ->
+            case Repo.delete(target_member) do
+              {:ok, _} ->
+                send_resp(conn, :no_content, "")
+              {:error, changeset} ->
                 conn
-                |> put_status(:forbidden)
-                |> render(Thegm.ErrorView, "error.json", errors: ["Admins cannot be removed from the group"])
+                |> put_status(:internal_server_error)
+                |> render(Thegm.ErrorView, "error.json", errors: Enum.map(changeset.errors, fn {k, v} -> Atom.to_string(k) <> ": " <> elem(v, 0) end))
             end
         end
     end
