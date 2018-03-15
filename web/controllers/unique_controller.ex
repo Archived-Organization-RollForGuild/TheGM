cbd89335a56b3092b9df02e5006fef9bacf6cf94
Thomas Quigley Malcolm <tqmalcolm@gmail.com>
2018-03-15 15:41:48 -0500

    feature(unique): unique usernames without suggestions

--- web/controllers/unique_controller.ex
+++ web/controllers/unique_controller.ex
@@ -13,7 +13,22 @@ defmodule Thegm.UniqueController do
       true ->
         conn
         |> put_status(:bad_request)
-        |>render(Thegm.ErrorView, "error.json", errors: ["email: must be valid email address"])
+        |> render(Thegm.ErrorView, "error.json", errors: ["email: must be valid email address"])
     end
+  end
+
+  def show(conn, %{"username" => username}) do
+    cond do
+      Regex.match?(~r/^[a-zA-Z0-9\s'_-]+$/, username) ->
+        case Repo.one(from u in Thegm.Users, where: u.username == ^username ) do
+          nil ->
+            send_resp(conn, :no_content, "")
+          _ ->
+            send_resp(conn, :conflict, "")
+        end
+      true ->
+        conn
+        |> put_status (:bad_request)
+        |> render(Thegm.ErrorView, "error.json", errors: ["username: invalid format"])
   end
 end
