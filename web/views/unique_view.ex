942364ed99ad5f7932821543bbf5100f9c713ba9
Thomas Quigley Malcolm <tqmalcolm@gmail.com>
2018-03-15 16:43:15 -0500

    feature(unique): Check uniqueness of username and return suggestions if necessary

---
+++ web/views/unique_view.ex
@@ -0,0 +1,14 @@
+defmodule Thegm.UniqueView do
+  use Thegm.Web, :view
+
+  def render("suggestions.json", %{suggestions: suggestions}) do
+    %{
+      data: %{
+        type: "suggestions",
+        attributes: %{
+          suggestions: suggestions
+        }
+      }
+    }
+  end
+end
