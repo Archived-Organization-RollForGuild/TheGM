0361495c09d172b00f605d84126414b14c0122ec
Thomas Quigley Malcolm <tqmalcolm@gmail.com>
2018-03-22 10:57:23 -0500

    fix(groups_controller): remove unused variables

--- web/controllers/groups_controller.ex
+++ web/controllers/groups_controller.ex
@@ -101,8 +101,8 @@ defmodule Thegm.GroupsController do
         join_requests_query = case users_id do
           nil ->
             from gjr in Thegm.GroupJoinRequests, where: is_nil(gjr.users_id), order_by: [desc: gjr.inserted_at]
-          exists ->
-            join_requests_query = from gjr in Thegm.GroupJoinRequests, where: gjr.users_id == ^users_id, order_by: [desc: gjr.inserted_at]
+          _ ->
+            from gjr in Thegm.GroupJoinRequests, where: gjr.users_id == ^users_id, order_by: [desc: gjr.inserted_at]
         end
         cond do
           total > 0 ->
